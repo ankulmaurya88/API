9421aa42446a9d09e6545acd70b2cee981f0a350
ankulmaurya88 <ankulmaurya88@gmail.com>
2025-04-19 15:07:53 +0530

    Apr-19

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Students, Teachers
+
+# Register your models here
+admin.site.register(Students)
+admin.site.register(Teachers)
